b5a16b03fb7639b427103d162c8c41390529d7e0
Dustin D'Amour <103222840+Estatic-Inkbl0t@users.noreply.github.com>
2026-05-11 13:19:14 -0600

    fix: truecolors in bashrc for better doom emacs
    Changelog: fixed

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -17,7 +17,9 @@
 ;;   presentations or streaming.
 ;; - `doom-symbol-font' -- for symbols
 ;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
+(setq doom-symbol-font (font-spec :family "Symbols Nerd Font Mono"))
+(setq nerd-icons-font-family "DejaVuSansMono Nerd Font Mono")
+
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
@@ -32,11 +34,12 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-solarized-dark)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
+(setq scroll-margin 8)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -73,3 +76,4 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+(setq doom-leader-key ",")
--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -32,6 +32,7 @@
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        dashboard         ; a nifty splash screen for Emacs
+       (doom +icons)     ; This enables the icon framework
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
